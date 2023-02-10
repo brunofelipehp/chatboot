af7b83ad9a6eeeb7b89dbf87b6b22dbf625480b4
Bruno Felipe <brunofelipehp8@gmail.com>
2023-02-10 17:11:07 -0300

    time send message

--- main.js
+++ main.js
@@ -11,26 +11,14 @@ const messagesContainer = [
 const dateTitle = dayjs().format("HH:mm");
 hourTitle.textContent = dateTitle;
 
-const arr = [1, 4, 3, 5, 2, 6, 7, 8];
-
-arr.forEach((as) => {
-  setInterval(function () {
-    //  return console.log(as);
-  }, 3000);
-});
-
-messagesContainer.forEach((message) => {
-  chatMessage(message);
-});
+if (messages.children.length === 0) {
+  chatMessage(messagesContainer[0]);
+}
 
 if (messages.children.length > 0) {
   messages.lastElementChild.scrollIntoView();
 }
-/*
-setTimeout(function () {
-  chatMessage(messagesContainer[1]);
-}, 9000);
-*/
+
 function sendMessages(event) {
   event.preventDefault();
 
@@ -40,7 +28,7 @@ function sendMessages(event) {
     const date = dayjs().format("HH:mm");
 
     let hourMessage = document.createElement("span");
-    hourMessage.textContent = date;
+    hourMessage.textContent = `Você - ${date}`;
 
     let messageText = document.createElement("p");
     messageText.textContent = msg;
@@ -54,6 +42,10 @@ function sendMessages(event) {
     messages.lastElementChild.scrollIntoView();
     inputMessage.value = "";
   }
+
+  if (messages.children.length === 2) {
+    chatMessage(messagesContainer[1]);
+  }
 }
 
 function chatMessage(msg) {
@@ -61,7 +53,7 @@ function chatMessage(msg) {
     const date = dayjs().format("HH:mm");
 
     let hourMessage = document.createElement("span");
-    hourMessage.textContent = date;
+    hourMessage.textContent = `Mayk Brito - ${date}`;
 
     let messageText = document.createElement("p");
     messageText.textContent = msg;
@@ -76,5 +68,3 @@ function chatMessage(msg) {
 }
 
 addEventListener("submit", sendMessages);
-//E se a gente fizesse um chat moderno e responsivo em apenas uma
-//semana?
